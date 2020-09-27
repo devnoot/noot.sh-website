6bd38a2b8281582461d359c3fd63ac55e2839f1b
devnoot <devnoot@gmail.com>
2020-09-27 09:16:25 -0400

    Changed the text color to white for navbar elements

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent } from 'react'
 import {
   AppBar,
   Toolbar,
@@ -8,40 +8,44 @@ import {
   Typography,
   makeStyles,
   AppBarProps,
-} from "@material-ui/core";
-import { Menu } from "@material-ui/icons";
+} from '@material-ui/core'
+import { Menu } from '@material-ui/icons'
 
 const MenuIcon = (props: IconButtonProps) => (
   <IconButton {...props}>
     <Menu />
   </IconButton>
-);
+)
 
 const useStyles = makeStyles((theme) => ({
-  "@keyframes gradient": {
-    "0%": {
-      backgroundPosition: "0% 50%",
+  '@keyframes gradient': {
+    '0%': {
+      backgroundPosition: '0% 50%',
     },
-    "50%": {
-      backgroundPosition: "100% 50%",
+    '50%': {
+      backgroundPosition: '100% 50%',
     },
-    "100%": {
-      backgroundPosition: "0% 50%",
+    '100%': {
+      backgroundPosition: '0% 50%',
     },
   },
   root: {
     background: `linear-gradient(-90deg, ${theme.palette.primary.main}, ${theme.palette.secondary.main})`,
-    backgroundSize: "400% 400%",
+    backgroundSize: '400% 400%',
     boxShadow: theme.shadows[15],
-    animation: "$gradient 15s infinite",
+    animation: '$gradient 15s infinite',
   },
   title: {
     flexGrow: 1,
+    color: 'white',
   },
-}));
+  button: {
+    color: 'white',
+  },
+}))
 
 export const Navbar: FunctionComponent = (props: AppBarProps) => {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <AppBar position="static" className={classes.root} {...props}>
@@ -50,8 +54,10 @@ export const Navbar: FunctionComponent = (props: AppBarProps) => {
         <Typography variant="h6" className={classes.title}>
           noot.sh
         </Typography>
-        <Button color="inherit">Login</Button>
+        <Button color="inherit" className={classes.button}>
+          Login
+        </Button>
       </Toolbar>
     </AppBar>
-  );
-};
+  )
+}
